9395fa562388a5c3c0138a9503decfd958585c65
Richie-Z <zakariarichie@yahoo.com>
2022-08-09 20:24:05 +0700

    add p3

---
+++ p3/biodata.py
@@ -0,0 +1,16 @@
+print("Pertemuan Ketiga")
+print("*"*20)
+
+nama = input("Masukan Nama: ")
+nim = input("Masukan NIM: ")
+umur = input("Masukan Umur: ")
+angkatan = input("Masukan Angkatan: ")
+
+print("\n\n")
+print("*"*20)
+print("BIODATA SINGKAT")
+print("*"*20)
+print("NAMA \t\t: %s" % (nama))
+print("NIM \t\t: %s" % (nim))
+print("UMUR \t\t: %s" % (umur))
+print("ANGKATAN \t: %s" % (angkatan))
